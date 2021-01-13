cd56681e70915d6f3a2ee0211e68633e547e126d
caroline-e-miller <caroline.miller@ymail.com>
2021-01-13 12:13:41 -0600

    last activity update

--- 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -4,11 +4,20 @@ var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 var signUpButton = document.querySelector("#sign-up");
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
-  
-  // TODO: Create user object from submissio
+
+  // TODO: Create user object from submission
+  var userObject = {
+    firstName: firstNameInput.value.trim(),
+    lastName: lastNameInput.value.trim(),
+    email: emailInput.value.trim(),
+    password: passwordInput.value.trim()
+  };
+  console.log('userObject: ', userObject);
 
   // TODO: Set new submission to local storage 
-  
+  localStorage.setItem('userObject', JSON.stringify(userObject));
+
 });
+var obj = localStorage.getItem("userObject");
