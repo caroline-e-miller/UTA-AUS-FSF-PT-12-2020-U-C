d9d2b0982497b98f9023e2c357f9440e48527573
caroline-e-miller <caroline.miller@ymail.com>
2021-03-17 19:55:56 -0400

    more activities

--- 11-express/01-Activities/15-HotRestaurant/Solved/routes/apiRoutes.js
+++ 11-express/01-Activities/15-HotRestaurant/Solved/routes/apiRoutes.js
@@ -2,6 +2,10 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 
+// `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
+
 const tableData = require('../data/tableData');
 const waitListData = require('../data/waitinglistData');
 
